b03ccfbc2e9739b1f3821055091b1959594154dc
eurashin <eura.shin@gmail.com>
2020-02-04 10:12:58 -0500

    change

--- app/src/main/java/com/example/reflect/FoodListActivity.java
+++ app/src/main/java/com/example/reflect/FoodListActivity.java
@@ -1,10 +1,18 @@
 package com.example.reflect;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.content.Intent;
 import android.database.Cursor;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.widget.ListView;
+import android.widget.SearchView;
 import android.widget.SimpleCursorAdapter;
 
 import com.example.reflect.database.DBManager;
@@ -23,19 +31,33 @@ public class FoodListActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_food_list);
 
+        // Open the SQLite Database
         dbManager = new DBManager(this);
         dbManager.open();
-        Cursor cursor = dbManager.fetchItems();
 
-
-        final String[] from = new String[] {"name"};
-        final int[] to = new int[] {R.id.food_cursor_name};
-        adapter = new SimpleCursorAdapter(this, R.layout.food_view_cursor, cursor, from, to, 0);
+        adapter = new SimpleCursorAdapter(this,
+                R.layout.food_view_cursor,
+                dbManager.fetchItems(),
+                new String[] {"name"},
+                new int[] {R.id.food_cursor_name},
+                0);
         adapter.notifyDataSetChanged();
 
         mListView = (ListView) findViewById(R.id.food_list_view);
         mListView.setAdapter(adapter);
+
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main, menu);
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.food_search).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+        searchView.setIconifiedByDefault(false);
+
+        return true;
+    }
 
 }
